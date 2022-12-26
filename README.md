343b61b22ff805072fc36c7afbc64f0596af3fee
codenuk <supagorn00001@gmail.com>
2022-12-26 20:46:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+yarn run electron:serve สำหรับ Test run in local
+yarn run electron:build สำหรับ build สร้าง File Exe for install in window
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mt-2">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
